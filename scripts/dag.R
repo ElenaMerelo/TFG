7833f2107f52587a54b14030f927fed08c04c1a5
JJ Merelo <jjmerelo@gmail.com>
2022-08-25 20:21:31 +0200

    Termina script, #66

--- scripts/dag.R
+++ scripts/dag.R
@@ -2,3 +2,6 @@ library(igraph)
 
 data <- read.csv("../data/dag-data.csv",header=FALSE)
 a.graph <- graph.data.frame(data.frame(data$V1,data$V2))
+png(filename="../doc/dag.png")
+plot(a.graph)
+dev.off()
