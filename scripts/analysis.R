6061ad6891465fb458c7a19cb6838654a5357a96
Elena Merelo <nenamer@gmail.com>
2022-08-31 22:52:31 +0200

    voy añadiendo, avanza implementación

--- scripts/analysis.R
+++ scripts/analysis.R
@@ -1,67 +1,82 @@
-library(SBpitch)
 library(StatsBombR)
 library(igraph)
 library(tidyverse)
 library(dplyr)
 
 # extraemos los eventos
-
 events_eng <- readRDS("scripts/data_england.Rds")
 events_norw <- readRDS("scripts/data_norway.Rds")
 
 #quitamos NA
-events_eng$pass.outcome.name <- ifelse(is.na(events_eng$pass.outcome.name), 
+events_eng$pass.outcome.name <- ifelse(is.na(events_eng$pass.outcome.name),
 "Complete", as.character(events_eng$pass.outcome.name))
 events_eng$pass.type.name <- ifelse(is.na(events_eng$pass.type.name),
 "-", as.character(events_eng$pass.type.name))
 
 # me quedo con los pases, que tienen id 30
-events_eng <- events_eng %>% filter(type.id == 30, 
-possession_team.name == "England Women's")
+events_eng <- events_eng %>% filter(type.id == 30,
+team.name == "England Women's")
 
 #quitamos lo que no son estrictamente pases, o no tengan éxito
-passes_eng <- events_eng %>% filter(pass.type.name != "Goal Kick" &
-pass.type.name != "Corner" & pass.type.name != "Throw-in" &
-pass.type.name != "Free Kick" & pass.type.name != "Kick Off" &
+passes_eng <- events_eng %>% filter(pass.type.name == "-" &
 pass.outcome.name == "Complete")
 
+# partido eng-norw, desde eng
+#match_eng <- passes_eng %>% filter(match_id == 3835327)
 # análogo para Noruega
-
 events_norw$pass.outcome.name <- ifelse(is.na(events_norw$pass.outcome.name),
 "Complete", as.character(events_norw$pass.outcome.name))
 events_norw$pass.type.name <- ifelse(is.na(events_norw$pass.type.name),
 "-", as.character(events_norw$pass.type.name))
 
-events_norw <- events_norw %>% filter(type.id == 30, 
-possession_team.name == "Norway Women's")
+events_norw <- events_norw %>% filter(type.id == 30,
+team.name == "Norway Women's")
 
-passes_norw <- events_norw %>% filter(pass.type.name != "Goal Kick" &
-pass.type.name != "Corner" & pass.type.name != "Throw-in" &
-pass.type.name != "Free Kick" & pass.type.name != "Kick Off" &
+passes_norw <- events_norw %>% filter(pass.type.name == "-" &
 pass.outcome.name == "Complete")
 
-## reduzco las tablas
-#passes_eng <- passes_eng %>% select(timestamp, location, player.name,
-#pass.length, pass.angle, pass.end_location, pass.recipient.name,
-#location.x, location.y, pass.end_location.x, pass.end_location.y)
-
-#view(passes_eng)
-
+# partido eng -norw, desde norw
+# match_norw <- passes_norw %>% filter(match_id == 3835327)
 # me quedo solo con quién pasa y quién recibe, localización
 passes_eng <- passes_eng %>% select(player.name, pass.recipient.name)
-
-#passes_norw <- passes_norw %>% select(timestamp, location, player.name,
-#pass.length, pass.angle, pass.end_location, pass.recipient.name,
-#location.x, location.y, pass.end_location.x, pass.end_location.y)
-#view(passes_norw)
 passes_norw <- passes_norw %>% select(player.name, pass.recipient.name)
 
-# creamos el campo
-p <- create_Pitch(grass_colour = "#224C56",
-line_colour =  "#B3CED9",
-background_colour = "#224C56",
-goal_colour = "#15393D")
 
+# calculamos entropía total de eng en euro 2022
 england <- graph.data.frame(data.frame(passes_eng$player.name,
-passes_eng$pass.recipient.name), directed = TRUE)
+passes_eng$pass.recipient.name), directed = FALSE)
+png(filename = "scripts/plot_england")
 plot(england)
+dev.off()
+england.simplified <- england
+E(england.simplified)$weight <- 1
+england.simplified <- simplify(england.simplified, edge.attr.comb=list(weight="sum"))
+
+
+england.simplified$diversity <- diversity(england.simplified)
+png(filename = "scripts/plot_england_simpl")
+plot(england.simplified,
+     vertex.size=england.simplified$diversity*10,
+     edge.width=E(england.simplified)$weight/5)
+dev.off()
+view(england.simplified$diversity)
+
+# análogo para noruega
+
+norway <- graph.data.frame(data.frame(passes_norw$player.name,
+passes_norw$pass.recipient.name), directed = FALSE)
+png(filename = "scripts/plot_norw")
+plot(norway)
+norway.simplified <- norway
+E(norway.simplified)$weight <- 1
+norway.simplified <- simplify(norway.simplified, edge.attr.comb=list(weight="sum"))
+dev.off()
+
+norway.simplified$diversity <- diversity(norway.simplified)
+png(filename = "scripts/plot_norw_simpl")
+plot(norway.simplified,
+     vertex.size=norway.simplified$diversity*10,
+     edge.width=E(norway.simplified)$weight/5)
+dev.off()
+view(norway.simplified$diversity)
+
